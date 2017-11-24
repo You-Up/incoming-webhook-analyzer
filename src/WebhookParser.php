305664cc3852dcf685dc7b5c5daa308bf5c619fc
Thomas Genin <xt6.thomas.genin@gmail.com>
2017-11-24 23:11:44 +0100

    Accept an error callback to the parsing mechanism to allow logging

--- src/WebhookParser.php
+++ src/WebhookParser.php
@@ -9,16 +9,23 @@ class Main {
      * @param \Illuminate\Http\Request $request
      * @return null|\WebhookParser\WebhookIncident
      */
-    static function run(Request $request) {
+    static function run(Request $request, $errorCallback = null) {
         $parserProviders = new ParserProvider(true);
         foreach ($parserProviders->getClassList() as $providerClass) {
-            /** @var \WebhookParser\Parser $cls */
-            $cls = new $providerClass($request);
+            try {
+                /** @var \WebhookParser\Parser $cls */
+                $cls = new $providerClass( $request );
 
-            if ($cls->isMatch()) {
-                $incident = $cls->extract();
-                $incident->setParser($cls->companyName(), $cls->version());
-                return $incident;
+                if ($cls->isMatch()) {
+                    $incident = $cls->extract();
+                    $incident->setParser( $cls->companyName(), $cls->version() );
+
+                    return $incident;
+                }
+            } catch(\Exception $e) {
+                if (is_callable($errorCallback) ) {
+                    $errorCallback($e);
+                }
             }
         }
         return null;
